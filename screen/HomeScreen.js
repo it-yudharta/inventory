8e4d567e39e59809153bd3b4993acecbb2f6275d
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2023-12-06 11:50:41 +0700

    list data

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,22 +1,62 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+const Item = ({name}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{name}</Text>
+  </View>
+);
 
 const HomeScreen = () => {
-    return (
-        <View style={styles.container}>
-          <Text>Aplikasi Inventory</Text>
-          <StatusBar style="auto" />
-        </View>
-    );    
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getDatas = async () => {
+    try {
+      const response = await fetch('https://api.restful-api.dev/objects');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDatas();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator style={styles.container}/>
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({item}) => <Item name={item.name} />}
+          keyExtractor={item => item.id}
+        />
+      )}
+    </SafeAreaView>
+  );
 }
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 4,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
 });
   
 export default HomeScreen
